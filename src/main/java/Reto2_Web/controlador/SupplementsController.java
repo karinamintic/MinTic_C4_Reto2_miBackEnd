be87c5910bdbc66ce69552fff585b7eba9e7fd12
karinamintic <karina.arias.mt@correo.usa.edu.co>
2021-12-10 01:18:47 -0500

    Final Final

--- src/main/java/Reto2_Web/controlador/SupplementsController.java
+++ src/main/java/Reto2_Web/controlador/SupplementsController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author USUARIO
  */
 @RestController
-@RequestMapping("/api/supplements")
+@RequestMapping("/api/cosmetics")
 @CrossOrigin("*")
 public class SupplementsController {
        @Autowired
